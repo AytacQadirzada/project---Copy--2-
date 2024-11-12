726a922e18e0021178d8376310058875af5c62b4
AytacQadirzada <qadirzadaaytac@gmail.com>
2024-11-12 13:52:53 +0400

    finish

--- app.js
+++ app.js
@@ -58,86 +58,7 @@ hoverPlus.addEventListener("click",() =>{
     input.classList.toggle("dis-none");
 })
 hoverText.addEventListener('click', () => {
-    input.classList.add("dis-none");
-
-        if (addInput.value.trim() === "") {
-            return;
-    }
-    else{
-        let mainBlock = document.createElement("div");
-        mainBlock.classList.add("main-block");
-
-        let blockNumberAndText = document.createElement("div");
-        blockNumberAndText.classList.add("block-number-text");
-        
-        block.append(mainBlock);
-        mainBlock.append(blockNumberAndText);
-        block.classList.add("block-element");
-
-        let number = document.createElement("div");
-        blockNumberAndText.append(number);
-        number.classList.add("number");
-
-        let blockElementText = document.createElement("p");
-        blockNumberAndText.append(blockElementText);
-        blockElementText.classList.add("block-element-text");  
-        blockElementText.textContent = addInput.value;
-
-        let imgBlock = document.createElement("div");
-        imgBlock.classList.add("img-block");
-        mainBlock.append(imgBlock);
-
-        let imgGray = document.createElement("img");
-        imgGray.classList.add("clear-gray-element");
-        imgGray.src = "./sekiller/clear_gray.svg";
-
-        let imgPurple = document.createElement("img");
-        imgPurple.classList.add("clear-purple-element", "dis-none");
-        imgPurple.src = "./sekiller/clear_purple.svg";
-
-        imgBlock.append(imgGray);
-        imgBlock.append(imgPurple);
-
-        block.classList.remove("dis-none");
-        block.classList.add('new-block');
-
-        imgGray.addEventListener("mouseover", () => {
-            imgGray.classList.toggle('dis-none');
-            imgPurple.classList.toggle('dis-none');
-        });
-
-        imgGray.addEventListener("mouseout", () => {
-            imgGray.classList.toggle('dis-none');
-            imgPurple.classList.toggle('dis-none');
-        });
-
-        imgPurple.addEventListener('click', () => {
-            mainBlock.remove();
-            if (block.innerHTML === "") {
-                block.classList.toggle("dis-none");
-                block.classList.toggle("new-block");
-            }
-            updateNumbers();
-        });
-
-        imgGray.addEventListener('touchstart', () => {
-            mainBlock.remove();
-            if (block.innerHTML === "") {
-                block.classList.toggle("dis-none");
-                block.classList.toggle("new-block");
-            }
-            updateNumbers();
-        });
-        
-        updateNumbers(); 
-    }
-    addInput.value="";
-
-});
-
-let mainText=document.querySelector('.add-text');
-let mainPlus=document.querySelector('.add-plus');
-mainText.addEventListener('touchstart', () => {
+    
     if (addInput.value.trim() === "") {
         return;
     }
@@ -209,8 +130,89 @@ mainText.addEventListener('touchstart', () => {
         });
         
         updateNumbers(); 
-        addInput.value = "";
+        addInput.value="";
     }
+
+});
+
+let mainText=document.querySelector('.add-text');
+let mainPlus=document.querySelector('.add-plus');
+mainText.addEventListener('touchstart', () => {
+    
+    if (addInput.value.trim() === "") {
+        return;
+    }
+    else{
+        input.classList.add("dis-none");
+        let mainBlock = document.createElement("div");
+        mainBlock.classList.add("main-block");
+
+        let blockNumberAndText = document.createElement("div");
+        blockNumberAndText.classList.add("block-number-text");
+        
+        block.append(mainBlock);
+        mainBlock.append(blockNumberAndText);
+        block.classList.add("block-element");
+
+        let number = document.createElement("div");
+        blockNumberAndText.append(number);
+        number.classList.add("number");
+
+        let blockElementText = document.createElement("p");
+        blockNumberAndText.append(blockElementText);
+        blockElementText.classList.add("block-element-text");  
+        blockElementText.textContent = addInput.value;
+
+        let imgBlock = document.createElement("div");
+        imgBlock.classList.add("img-block");
+        mainBlock.append(imgBlock);
+
+        let imgGray = document.createElement("img");
+        imgGray.classList.add("clear-gray-element");
+        imgGray.src = "./sekiller/clear_gray.svg";
+
+        let imgPurple = document.createElement("img");
+        imgPurple.classList.add("clear-purple-element", "dis-none");
+        imgPurple.src = "./sekiller/clear_purple.svg";
+
+        imgBlock.append(imgGray);
+        imgBlock.append(imgPurple);
+
+        block.classList.remove("dis-none");
+        block.classList.add('new-block');
+
+        imgGray.addEventListener("mouseover", () => {
+            imgGray.classList.toggle('dis-none');
+            imgPurple.classList.toggle('dis-none');
+        });
+
+        imgGray.addEventListener("mouseout", () => {
+            imgGray.classList.toggle('dis-none');
+            imgPurple.classList.toggle('dis-none');
+        });
+
+        imgPurple.addEventListener('click', () => {
+            mainBlock.remove();
+            if (block.innerHTML === "") {
+                block.classList.toggle("dis-none");
+                block.classList.toggle("new-block");
+            }
+            updateNumbers();
+        });
+
+        imgGray.addEventListener('touchstart', () => {
+            mainBlock.remove();
+            if (block.innerHTML === "") {
+                block.classList.toggle("dis-none");
+                block.classList.toggle("new-block");
+            }
+            updateNumbers();
+        });
+        
+        updateNumbers(); 
+        addInput.value="";
+    }
+
 });
 
 mainPlus.addEventListener("touchstart",() =>{
